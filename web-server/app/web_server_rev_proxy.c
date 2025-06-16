067cdb67a48b82aeac8f3c6f754d836293564d10
Daniel Myhrman <74043942+danielmyh@users.noreply.github.com>
2025-06-16 13:14:34 +0200

    Update web-server example to use Civetweb
    Co-authored-by: Isak Jakobsson <isakj@axis.com>
    Co-authored-by: Stiv Abdullwahed <stiva@axis.com>
    Co-authored-by: Daniel Myhrman <danielmy@axis.com>

--- web-server/app/web_server_rev_proxy.c
+++ web-server/app/web_server_rev_proxy.c
@@ -14,107 +14,64 @@
  * limitations under the License.
  */
 
-#include <glib-unix.h>
-#include <glib.h>
-#include <libmonkey.h>
-#include <stdio.h>
+#include "civetweb.h"
+#include <signal.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
 #include <time.h>
 #include <unistd.h>
 
-#define APP_NAME "web_server_rev_proxy"
+#define PORT "2001"
+volatile sig_atomic_t application_running = 1;
 
-static char html_buffer[8192];
-
-static gboolean signal_handler(gpointer loop) {
-    g_main_loop_quit((GMainLoop*)loop);
-    syslog(LOG_INFO, "Application was stopped by SIGTERM or SIGINT.");
-    return G_SOURCE_REMOVE;
-}
-
-// Print an error to syslog and exit the application if a fatal error occurs.
-__attribute__((noreturn)) __attribute__((format(printf, 1, 2))) static void
-panic(const char* format, ...) {
-    va_list arg;
-    va_start(arg, format);
-    vsyslog(LOG_ERR, format, arg);
-    va_end(arg);
-    exit(1);
-}
-
-// Compose a simple HTML page
-static int serve_web_page(const mklib_session* session,
-                          const char* vhost,
-                          const char* cgi_path,
-                          const char* get,
-                          unsigned long get_len,
-                          const char* post,
-                          unsigned long post_len,
-                          unsigned int* status,
-                          const char** content,
-                          unsigned long* content_len,
-                          char* header) {
-    (void)session;
-    (void)vhost;
-    (void)get;
-    (void)get_len;
-    (void)post;
-    (void)post_len;
+static void stop_application(int status) {
     (void)status;
-    time_t t;
-
-    // Register page type
-    sprintf(header, "Content-type: text/html");
-
-    // Compose page content start
-    sprintf(html_buffer, "<html><body>");
-
-    // Compose page body
-    strcat(html_buffer, "<h1>ACAP application with reverse proxy web server</h1>");
-    strcat(html_buffer, "<pre>");
-    strcat(html_buffer, "<br>Application name:      ");
-    strcat(html_buffer, APP_NAME);
-    strcat(html_buffer, "<br>Reverse proxy path:    ");
-    strcat(html_buffer, cgi_path);
-    strcat(html_buffer, "<br>Request timestamp:     ");
-    time(&t);
-    strcat(html_buffer, ctime(&t));
-
-    // Compose page content end
-    strcat(html_buffer, "</pre>");
-    strcat(html_buffer, "</body></html>");
-
-    // Write buffer to callback pointer
-    *content     = html_buffer;
-    *content_len = strlen(html_buffer);
-
-    return MKLIB_TRUE;
+    application_running = 0;
 }
 
-static cb_data callback_function = serve_web_page;
+static int RootHandler(struct mg_connection* conn, void* cb_data __attribute__((unused))) {
+    mg_printf(conn,
+              "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: "
+              "close\r\n\r\n");
+    FILE* html_file   = fopen("html/index.html", "r");
+    int FILE_STR_SIZE = 128;
+    char file_str[FILE_STR_SIZE];
+    while (fgets(file_str, FILE_STR_SIZE, html_file)) {
+        mg_printf(conn, "%s", file_str);
+    }
+    fclose(html_file);
+    return 1;
+}
 
 int main(void) {
-    GMainLoop* loop = NULL;
+    signal(SIGTERM, stop_application);
+    signal(SIGINT, stop_application);
 
-    openlog(APP_NAME, LOG_PID, LOG_USER);
-    loop = g_main_loop_new(NULL, FALSE);
+    const char* options[] =
+        {"listening_ports", PORT, "request_timeout_ms", "10000", "error_log_file", "error.log", 0};
 
-    mklib_ctx context = mklib_init(NULL, 0, 0, NULL);
-    if (!context)
-        panic("Could not crete web server context, mklib_init failed.");
+    struct mg_callbacks callbacks;
+    struct mg_context* context;
 
-    mklib_callback_set(context, MKCB_DATA, callback_function);
-    syslog(LOG_INFO, "Callback has been registered");
+    mg_init_library(0);
 
-    mklib_start(context);
+    memset(&callbacks, 0, sizeof(callbacks));
+
+    context = mg_start(&callbacks, 0, options);
     syslog(LOG_INFO, "Server has started");
 
-    g_unix_signal_add(SIGTERM, signal_handler, loop);
-    g_unix_signal_add(SIGINT, signal_handler, loop);
-    g_main_loop_run(loop);
+    mg_set_request_handler(context, "/", RootHandler, 0);
 
-    mklib_stop(context);
-    g_main_loop_unref(loop);
+    if (context == NULL) {
+        syslog(LOG_INFO, "Something went wrong when starting the web server.\n");
+        return EXIT_FAILURE;
+    }
+
+    while (application_running) {
+        sleep(1);
+    }
+
+    return EXIT_SUCCESS;
 }
