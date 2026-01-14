2f0618594c85b06cd053226ffa88a3d5af6473e9
Jimmy Rubin <jimmy.rubin@axis.com>
2026-01-14 10:01:16 +0100

    vdoencodeclient: Add support for AVIF
    Co-authored-by: Jimmy Rubin <jimmyrn@axis.com>
    Co-authored-by: Isak Jakobsson <isakj@axis.com>

--- vdostream/app/vdoencodeclient.c
+++ vdostream/app/vdoencodeclient.c
@@ -27,7 +27,7 @@
  * order: format frames output.
  *
  * First argument, format, is a string describing the video compression format.
- * Possible values are h264 (default), h265, jpeg, nv12, and y800.
+ * Possible values are avif, h264 (default), h265, jpeg, nv12, and y800.
  *
  * Second argument, frames, is an integer for number of captured frames.
  *
@@ -61,9 +61,8 @@
 #include <stdlib.h>
 #include <syslog.h>
 
-#define VDO_CLIENT_ERROR g_quark_from_static_string("vdo-client-error")
+#include "panic.h"
 
-static VdoStream* stream;
 static gboolean shutdown       = FALSE;
 static const gchar* param_desc = "";
 static const gchar* summary    = "Encoded video client";
@@ -78,6 +77,9 @@ static void handle_sigint(int signum) {
 static void print_frame(VdoFrame* frame) {
     gchar* frame_type;
     switch (vdo_frame_get_frame_type(frame)) {
+        case VDO_FRAME_TYPE_AVIF:
+            frame_type = "avif";
+            break;
         case VDO_FRAME_TYPE_H264_IDR:
         case VDO_FRAME_TYPE_H265_IDR:
         case VDO_FRAME_TYPE_H264_I:
@@ -106,8 +108,10 @@ static void print_frame(VdoFrame* frame) {
 }
 
 // Set vdo format from input parameter
-static gboolean set_format(VdoMap* settings, gchar* format, GError** error) {
-    if (g_strcmp0(format, "h264") == 0) {
+static void set_format(VdoMap* settings, gchar* format) {
+    if (g_strcmp0(format, "avif") == 0) {
+        vdo_map_set_uint32(settings, "format", VDO_FORMAT_AVIF);
+    } else if (g_strcmp0(format, "h264") == 0) {
         vdo_map_set_uint32(settings, "format", VDO_FORMAT_H264);
     } else if (g_strcmp0(format, "h265") == 0) {
         vdo_map_set_uint32(settings, "format", VDO_FORMAT_H265);
@@ -120,32 +124,51 @@ static gboolean set_format(VdoMap* settings, gchar* format, GError** error) {
         vdo_map_set_uint32(settings, "format", VDO_FORMAT_YUV);
         vdo_map_set_string(settings, "subformat", "Y800");
     } else {
-        g_set_error(error,
-                    VDO_CLIENT_ERROR,
-                    VDO_ERROR_NOT_FOUND,
-                    "Format \"%s\" is not supported\n",
-                    format);
-        return FALSE;
+        panic("%s: Format \"%s\" is not supported\n", __func__, format);
     }
+}
 
-    return TRUE;
+static void save_frame_to_file(VdoBuffer* buffer, FILE* dest_f) {
+    // Lifetimes of buffer and frame are linked, no need to free frame
+    VdoFrame* frame = vdo_buffer_get_frame(buffer);
+
+    print_frame(frame);
+
+    gpointer data = vdo_buffer_get_data(buffer);
+    if (!data)
+        panic("%s: Failed to get data: %m", __func__);
+
+    if (!fwrite(data, vdo_frame_get_size(frame), 1, dest_f))
+        panic("%s: Failed to write frame: %m", __func__);
+}
+
+static int handle_vdo_failed(GError* error) {
+    // Maintenance/Installation in progress (e.g. Global-Rotation)
+    if (vdo_error_is_expected(&error)) {
+        syslog(LOG_INFO, "Expected vdo error %s", error->message);
+        return EXIT_SUCCESS;
+    } else {
+        panic("Unexpected vdo error %s", error->message);
+    }
+    return EXIT_FAILURE;
 }
 
 /**
  * Main function that starts a stream with the following options:
  *
- * --format [h264, h265, jpeg, nv12, y800]
+ * --format [avif, h264, h265, jpeg, nv12, y800]
  * --frames [number of frames]
  * --output [output filename]
  */
 int main(int argc, char* argv[]) {
-    g_autoptr(GError) error    = NULL;
-    g_autoptr(VdoMap) settings = NULL;
-    g_autoptr(VdoMap) info     = NULL;
-    gchar* format              = "h264";
-    guint frames               = G_MAXUINT;
-    gchar* output_file         = "/dev/null";
-    FILE* dest_f               = NULL;
+    g_autoptr(GError) error     = NULL;
+    g_autoptr(VdoMap) settings  = NULL;
+    g_autoptr(VdoMap) info      = NULL;
+    g_autoptr(VdoStream) stream = NULL;
+    gchar* format               = "h264";
+    guint frames                = G_MAXUINT;
+    gchar* output_file          = "/dev/null";
+    FILE* dest_f                = NULL;
 
     GOptionEntry options[] = {
         {"format",
@@ -153,7 +176,7 @@ int main(int argc, char* argv[]) {
          0,
          G_OPTION_ARG_STRING,
          &format,
-         "format (h264, h265, jpeg, nv12, y800)",
+         "format (avif, h264, h265, jpeg, nv12, y800)",
          NULL},
         {"frames", 'n', 0, G_OPTION_ARG_INT, &frames, "number of frames", NULL},
         {"output", 'o', 0, G_OPTION_ARG_FILENAME, &output_file, "output filename", NULL},
@@ -169,40 +192,57 @@ int main(int argc, char* argv[]) {
 
     GOptionContext* context = g_option_context_new(param_desc);
     if (!context)
-        return -1;
+        panic("%s Context is null", __func__);
 
     g_option_context_set_summary(context, summary);
     g_option_context_add_main_entries(context, options, NULL);
     if (!g_option_context_parse(context, &argc, &argv, &error))
-        goto exit;
+        panic("%s Failed to use option_context_parse: %s", __func__, error->message);
 
     dest_f = fopen(output_file, "wb");
-    if (!dest_f) {
-        g_set_error(&error, VDO_CLIENT_ERROR, VDO_ERROR_IO, "open failed: %m");
-        goto exit;
-    }
+    if (!dest_f)
+        panic("%s open failed: %m", __func__);
 
-    if (signal(SIGINT, handle_sigint) == SIG_ERR) {
-        g_set_error(&error, VDO_CLIENT_ERROR, VDO_ERROR_IO, "Failed to install signal handler: %m");
-        goto exit;
-    }
+    if (signal(SIGINT, handle_sigint) == SIG_ERR)
+        panic("%s Failed to install signal handler: %m", __func__);
 
     settings = vdo_map_new();
-    if (!set_format(settings, format, &error))
-        goto exit;
+    set_format(settings, format);
 
     // Set default arguments
-    vdo_map_set_uint32(settings, "width", 640);
-    vdo_map_set_uint32(settings, "height", 360);
+    VdoPair32u resolution = {
+        .w = 640,
+        .h = 360,
+    };
+    vdo_map_set_pair32u(settings, "resolution", resolution);
+
+    // Use snapshot API when nbr of frames are 1
+    if (frames == 1) {
+        g_autoptr(VdoBuffer) buffer = vdo_stream_snapshot(settings, &error);
+        if (!buffer)
+            panic("%s: Failed to get snapshot: %s", __func__, error->message);
+        syslog(LOG_INFO,
+               "Starting stream: %s, %ux%u, 1 fps\n",
+               format,
+               vdo_map_get_uint32(settings, "width", 0),
+               vdo_map_get_uint32(settings, "height", 0));
+        save_frame_to_file(buffer, dest_f);
+        goto exit;
+    }
+
+    // When several frames should be retrieved
+    // Not to be used for AVIF
+    if (g_strcmp0(format, "avif") == 0)
+        panic("AVIF should not be used for more frames than one");
 
     // Create a new stream
     stream = vdo_stream_new(settings, NULL, &error);
     if (!stream)
-        goto exit;
+        panic("%s: Failed creating vdo stream: %s", __func__, error->message);
 
     info = vdo_stream_get_info(stream, &error);
     if (!info)
-        goto exit;
+        panic("%s: Failed to get vdo stream info: %s", __func__, error->message);
 
     syslog(LOG_INFO,
            "Starting stream: %s, %ux%u, %u fps\n",
@@ -213,7 +253,7 @@ int main(int argc, char* argv[]) {
 
     // Start the stream
     if (!vdo_stream_start(stream, &error))
-        goto exit;
+        panic("%s: Failed to start vdo stream : %s", __func__, error->message);
 
     // Loop until interrupt by Ctrl-C or reaching G_MAXUINT
     for (guint n = 0; n < frames; ++n) {
@@ -222,56 +262,30 @@ int main(int argc, char* argv[]) {
             goto exit;
 
         g_autoptr(VdoBuffer) buffer = vdo_stream_get_buffer(stream, &error);
-        if (!buffer && g_error_matches(error, VDO_ERROR, VDO_ERROR_NO_DATA))
+        if (!buffer && g_error_matches(error, VDO_ERROR, VDO_ERROR_NO_DATA)) {
+            g_clear_error(&error);
             continue;  // Transient error -> Retry
-
-        // Maintenance/Installation in progress (e.g. Global-Rotation)
-        if (!buffer && vdo_error_is_expected(&error)) {
-            // This may happen and then the stream needs to be created again
-            // and restarted
-            goto exit;
         }
 
-        // Error occurred
         if (!buffer)
-            goto exit;
+            return handle_vdo_failed(error);
 
-        // Lifetimes of buffer and frame are linked, no need to free frame
-        VdoFrame* frame = vdo_buffer_get_frame(buffer);
-
-        print_frame(frame);
-
-        gpointer data = vdo_buffer_get_data(buffer);
-        if (!data) {
-            g_set_error(&error, VDO_CLIENT_ERROR, 0, "Failed to get data: %m");
-            goto exit;
-        }
-
-        if (!fwrite(data, vdo_frame_get_size(frame), 1, dest_f)) {
-            g_set_error(&error, VDO_CLIENT_ERROR, 0, "Failed to write frame: %m");
-            goto exit;
-        }
+        save_frame_to_file(buffer, dest_f);
 
         // Release the buffer and allow the server to reuse it
-        if (!vdo_stream_buffer_unref(stream, &buffer, &error))
-            goto exit;
+        if (!vdo_stream_buffer_unref(stream, &buffer, &error)) {
+            if (!vdo_error_is_expected(&error))
+                panic("%s: Unexpected error: %s", __func__, error->message);
+            g_clear_error(&error);
+        }
     }
 
 exit:
-    // Ignore SIGINT and server maintenance
-    if (shutdown || vdo_error_is_expected(&error))
-        g_clear_error(&error);
-
-    gint ret = EXIT_SUCCESS;
-    if (error) {
-        syslog(LOG_INFO, "vdo-encode-client: %s\n", error->message);
-        ret = EXIT_FAILURE;
-    }
-
     if (dest_f)
         fclose(dest_f);
 
     g_option_context_free(context);
 
-    return ret;
+    syslog(LOG_INFO, "Exit %s", argv[0]);
+    return 0;
 }
